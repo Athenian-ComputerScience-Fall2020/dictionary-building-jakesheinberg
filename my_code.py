c28670982df4acf027659fb70a3bb6049b4fb333
jakesheinberg <23jsheinberg@athenian.org>
2020-10-21 19:05:51 +0000

    in class

--- my_code.py
+++ my_code.py
@@ -1,3 +1,9 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  
-
+name=input("please enter your name: ")
+age=input("please enter your age: ")
+grade=input("please enter your grade: ")
+school=input("please enter your school: ")
+directory={}
+directory.update({'name':name, 'age':age,'grade':grade,'school':school})
+for key_name, value_name in directory.items():
+    print(f"Your {key_name} is {value_name}")
